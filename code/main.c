bb8926e1e66eadb5a7fa0b2e1abdfaf3a98de18e
mks-a <8500434+mks-a@users.noreply.github.com>
2017-10-14 21:56:07 +0300

    Changes 20171014 3
    Changes 20171014 3

--- code/main.c
+++ code/main.c
@@ -2,8 +2,8 @@
 
 #include <avr/io.h>       	// this is always included in AVR programs
 #include <avr/interrupt.h>
-#ifndef F_CPU          // if F_CPU was not defined in Project -> Properties
-#define F_CPU 16000000UL     // define it now as 1 MHz unsigned long
+#ifndef F_CPU          		// if F_CPU was not defined in Project -> Properties
+#define F_CPU 16000000UL    // define it now as 16 MHz unsigned long
 #endif
 #include <util/delay.h>     // add this to use the delay function
 #include <avr/sleep.h>
@@ -20,12 +20,12 @@ int main(void) {
 		sleep_disable();
 	}
   
-	return(0);          // should never get here, this is to prevent a compiler warning
+	return(0);          	// should never get here, this is to prevent a compiler warning
 }
 
 void init(void)
 {
-	DDRB = 0x20;     // set pin 13 as output, others as input
+	DDRB = 0x20;     		// set pin 13 as output, others as input
 	
 	sei();
 }
@@ -36,18 +36,18 @@ void init_timer1(void)
 	TCCR1B = 0x05;
 	
 	// Clear pending interrupts
-	TIFR1 |= 0x01;		// Timer / Counter1 Interrupt flag register
+	TIFR1 |= 0x01;			// Timer / Counter1 Interrupt flag register
 	
 	// Set timer/counter
 	TCNT1 = 0xFFFF;
 	
 	// enable timer/counter interrupt
-	TIMSK1 = 0x01;		// overflow interrupt enable
+	TIMSK1 = 0x01;			// overflow interrupt enable
 }
 
 ISR(TIMER1_OVF_vect)
 {
-	PORTB ^= 0x20;   	// set pin 13 to LOW and back
+	PORTB ^= 0x20;   		// set pin 13 to LOW and back
 }
 
 ISR(BADISR_vect) 
